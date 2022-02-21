6577d969a2860caaffe9a3801e1afd5ed89ad8ca
antonjonn <jonninenanton@gmail.com>
2022-02-21 16:26:30 +0200

    aaaaaaaaaaaaa

--- Lejos/src/app/LineFollow.java
+++ Lejos/src/app/LineFollow.java
@@ -2,28 +2,62 @@ package app;
 import lejos.ev3.*;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.motor.*;
-import lejos.hardware.sensor.EV3IRSensor;
+import lejos.robotics.Color;
+
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorMode;
+/**
+ * @author 35840
+ *
+ */
 public class LineFollow extends Thread {
+	EV3ColorSensor	color;
+	float[]		sample;
+
 	DataExc DEobj;
-	private EV3IRSensor ss;
+	private EV3ColorSensor ss;
 	private EV3UltrasonicSensor us;
 	private final int colorPattern = 45;
 	
 	public LineFollow (DataExc DE) {
 		DEobj = DE;
+		UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
+	    UnregulatedMotor motorD = new UnregulatedMotor(MotorPort.D);
+	    motorA.setPower(-50);
+	    motorD.setPower(-50);
+//		ss = new EV3ColorSensor(SensorPort.S4);
+	    
 		
-		ss = new EV3IRSensor(SensorPort.S4);
 		us = new EV3UltrasonicSensor(SensorPort.S1);
-		//ss.setFloodlight(true);
+		
 	}
+	
+	 public void EV3ColorSensor(Port port) {
+	    EV3ColorSensor color = new EV3ColorSensor(SensorPort.S4);
+		color.setCurrentMode(Color.RED);
+		color.setFloodlight(true);
+		color.getRedMode();
+		double lightvalue = 
+		ss.setFloodlight(true);	
+	    }
+		
+	
+//	
+//	public float getRed() {
+//		color.fetchSample(sample, 0);
+//		return sample[0];
+//	}
 	public void run() {
 		//loputon 
+		
 		while(true) {
 			if (DEobj.getCMD() == 1) {
-				if(ss.readValue()< colorPattern) {
+//				int lightlevel = color.fetchSample(Sample, 0);
+				if(color.getRedMode() < colorPattern) {
 					MotorPort.A.controlMotor(0, 3);
 					MotorPort.D.controlMotor(80,1);
 				} else {
